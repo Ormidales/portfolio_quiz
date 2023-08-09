72a9fc7bab9ebc32bfb9b10bac104a985a51b836
Hugo Doueil <xdocontact@gmail.com>
2023-08-09 10:36:16 +0200

    Modifications en profondeur (09/08/2023 à 10h37) - Création d'un Timer permettant de limiter le temps de réponse

--- quiz/script.js
+++ quiz/script.js
@@ -12,38 +12,42 @@ const progressPercentage = document.querySelector(".progress-percentage");
 const maxLives = 3; // Nombre maximum de vies
 let remainingLives = maxLives; // Vies restantes du joueur
 const livesElement = document.querySelector(".lives");
+const timerElement = document.querySelector(".timer");
+let timeLeft = 30; // Temps initial en secondes
+let timerInterval; // Variable pour stocker l'instance de l'intervalle de la minuterie
 
 const quizData = [
     {
-        question: "En quête d'aventure et de gloire brillante, Dans quelle saga de jeux pouvez-vous plonger, Où Geralt de Riv, en terres de tourment, Chasse monstres féroces et secrets profonds ?",
-        answers: ["The Witcher", "Elder Scrolls", "Dragon Age", "Dark Souls"],
-        correct: 0,
-    },
-    {
-        question: "Quel studio de développement est responsable de la série de jeux The Elder Scrolls et Fallout ?",
-        answers: ["Ubisoft", "Blizzard Entertainment", "Bethesda Game Studios", "Square Enix"],
+        question: "Quel studio de développement est derrière la célèbre série de jeux \"The Elder Scrolls\" ?",
+        answers: ["BioWare", "CD Projekt Red", "Bethesda Game Studios", "Blizzard Entertainment"],
         correct: 2,
     },
     {
-        question: "Quel célèbre jeu de construction et de survie a été créé par Markus Persson et plus tard développé par Mojang Studios ?",
-        answers: ["Minecraft", "Terraria", "Roblox", "The Sims"],
-        correct: 0,
+        question: "Quel langage de programmation est largement utilisé pour créer des jeux vidéo ?",
+        answers: ["HTML", "Java", "C++", "Python"],
+        correct: 2,
     },
     {
-        question: "Quel est le symbole chimique de l'oxygène ?",
-        answers: ["O", "Ox", "O2", "Oz"],
-        correct: 0,
+        question: "Quelle année a marqué le lancement du légendaire jeu \"Super Mario Bros.\" sur la console NES de Nintendo ?",
+        answers: ["1983", "1985 ", "1990", "1992"],
+        correct: 1,
     },
     {
-        question: "Quel est le symbole chimique de l'oxygène ?",
-        answers: ["O", "Ox", "O2", "Oz"],
-        correct: 0,
+        question: "Quel célèbre jeu de survie pixelisé a été développé par le studio Mojang ?",
+        answers: ["Fortnite", "PUBG", "Rust", "Minecraft"],
+        correct: 3,
+    },
+    {
+        question: "Quelle entreprise est à l'origine de la réalisation du casque de réalité virtuelle \"Oculus Rift\" ?",
+        answers: ["Sony", "Microsoft", "Valve", "Oculus VR"],
+        correct: 3,
     },
     // Ajoute d'autres questions et réponses
 ];
 
 let currentQuestion = 0;
-let score = 0;
+let score = 0; // Score du joueur
+const baseScore = 10; // Score de base pour chaque question
 
 function updateScore(newScore) {
     score = newScore;
@@ -64,6 +68,47 @@ function updateLives() {
     livesElement.textContent = remainingLives;
 }
 
+function startTimer() {
+    clearInterval(timerInterval);
+    timerElement.textContent = timeLeft;
+    timerElement.style.display = "flex"; // Affiche le timer au début de chaque question
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        timerElement.textContent = timeLeft;
+
+        if (timeLeft >= 20) {
+            timerElement.style.backgroundColor = "#55ff3e"; // Fond vert entre 10 et 7 secondes
+        } else if (timeLeft >= 10) {
+            timerElement.style.backgroundColor = "#ffd63e"; // Fond jaune entre 6 et 4 secondes
+        } else {
+            timerElement.style.backgroundColor = "#ff3e3e"; // Fond rouge entre 3 et 0 secondes
+        }
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            timerElement.style.backgroundColor = "";
+            if (remainingLives > 1) {
+                remainingLives--;
+                updateLives();
+                currentQuestion++;
+                if (currentQuestion < quizData.length) {
+                    showQuestion(currentQuestion);
+                    updateProgressBar();
+                }
+            } else {
+                showResultMessage("Dommage ! Tu as perdu toutes tes vies. Essaie à nouveau !");
+                clearQuizContainer();
+            }
+        }
+
+        if (currentQuestion === quizData.length) {
+            clearInterval(timerInterval);
+            timerElement.style.backgroundColor = "";
+            timerElement.style.display = "none"; // Masque le timer lorsque le quiz est terminé
+        }
+    }, 1000);
+}
+
 function showQuestion(questionIndex) {
     const currentQuestionData = quizData[questionIndex];
     questionElement.textContent = currentQuestionData.question;
@@ -73,6 +118,9 @@ function showQuestion(questionIndex) {
     questionNumberElement.textContent = questionIndex + 1;
     totalQuestionsElement.textContent = quizData.length;
     updateProgressBar();
+    timeLeft = 30; // Réinitialise le temps pour chaque nouvelle question
+    timerElement.style.backgroundColor = "#55ff3e";
+    startTimer(); // Lance la minuterie pour la nouvelle question
 }
 
 function showResultMessage(message) {
@@ -123,7 +171,9 @@ function clearQuizContainer() {
 function checkAnswer(selectedAnswer) {
     const currentQuestionData = quizData[currentQuestion];
     if (selectedAnswer === currentQuestionData.correct) {
-        updateScore(score + 1);
+        const timeMultiplier = timeLeft / 10; // Calcul du multiplicateur de temps
+        const questionScore = Math.round(baseScore * timeMultiplier);
+        updateScore(score + questionScore); // Mise à jour du score avec le bonus de temps
         showFeedbackMessage(true);
     } else {
         showFeedbackMessage(false);
@@ -132,6 +182,9 @@ function checkAnswer(selectedAnswer) {
         if (remainingLives === 0) {
             showResultMessage("Dommage ! Tu as perdu toutes tes vies. Essaie à nouveau !");
             clearQuizContainer();
+            clearInterval(timerInterval);
+            timerElement.style.backgroundColor = "";
+            timerElement.style.display = "none"; // Masque le timer lorsque le quiz est terminé
         }
         return;
     }
@@ -141,8 +194,12 @@ function checkAnswer(selectedAnswer) {
         showQuestion(currentQuestion);
         updateProgressBar();
     } else {
-        showResultMessage(`Bravo ! Tu as terminé le quiz avec un score de ${score}/${quizData.length}`);
+        updateScore(score * remainingLives);
+        showResultMessage(`Bravo ! Tu as terminé le quiz avec un score de ${score}`);
         clearQuizContainer();
+        clearInterval(timerInterval);
+        timerElement.style.backgroundColor = "";
+        timerElement.style.display = "none"; // Masque le timer lorsque le quiz est terminé
     }
 }
 
